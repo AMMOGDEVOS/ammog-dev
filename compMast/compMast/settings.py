f312438f1954e5c292dfd65bcbbfef9a9d119716
Swathi-0 <105665956+Swathi-0@users.noreply.github.com>
2023-11-11 19:28:29 +0530

    Update settings.py

--- compMast/compMast/settings.py
+++ compMast/compMast/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pjkjpif88l&qe&fw1^5izv_y^gnam6ave@h5nda=5eq_05g!n6'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
